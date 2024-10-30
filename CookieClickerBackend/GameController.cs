325397588e2fc29743bbd00f161d8beef4b93e87
Rasmus <liltorp03@gmail.com>
2024-10-30 20:17:50 +0100

    Added initial files for Cookie Clicker project - backend + frontend

---
+++ CookieClickerBackend/GameController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class GameController : ControllerBase
+{
+    private static int cookieCount = 0;
+
+    [HttpPost("click")]
+    public IActionResult clickCookie()
+    {
+        cookieCount++;
+        return Ok(new { cookieCount });
+    }
+
+    [HttpGet("stats")]
+    public IActionResult refreshStats()
+    {
+        return Ok(new { cookieCount });
+    }
+}
