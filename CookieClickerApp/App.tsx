2383e4a00eb4e1dc5aa8bbda5ff5dea3ea94fd29
Rasmus <liltorp03@gmail.com>
2024-10-31 12:24:24 +0100

    Added pressable image & navbar

--- CookieClickerApp/App.tsx
+++ CookieClickerApp/App.tsx
@@ -1,27 +1,58 @@
 import React, { useState } from 'react'; 
-import { SafeAreaView, Text, StyleSheet, Button, View } from 'react-native';
+import {Text, Image, StyleSheet, Button, View, Pressable } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context'; // Import SafeAreaProviderx
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import axiosModule from './axios';
 
+
+const image = {uri: 'https://static.wikia.nocookie.net/cookieclicker/images/5/5a/PerfectCookie.png'};
+const Stack = createNativeStackNavigator();
 const App = () => {
   const [cookieCount, setCookieCount] = useState(0);
   const onButtonClick = async () => {
-  
       const response = await axiosModule.handleClick();
       setCookieCount(response.data.cookieCount);
   };
-
   return (
+    <SafeAreaProvider> 
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="NavBar"
+          component={NavBar}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
     <View style={styles.container}>
-      <Button 
-        title="Click for cookies" 
-        onPress={onButtonClick}
-      />
+      <View style={styles.button}>
+      <Pressable onPress={onButtonClick}>
+        <Image source={image} style={styles.image} resizeMode="contain" />
+      </Pressable>
+      </View>
       <Text style={styles.text}>You have {cookieCount} cookies</Text>
     </View>
+    </SafeAreaProvider>
+    
   );
 };
 
+const NavBar = () => {
+  return (
+    <SafeAreaProvider> 
+        </SafeAreaProvider>
+  );
+};
 const styles = StyleSheet.create({
+  button: {
+    //backgroundColor:"#64c6d1",
+    //borderRadius: 20,
+  },
+  image: {
+    justifyContent: 'center',
+    width: 200,
+    height: 200,
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
--- CookieClickerBackend/obj/Debug/net8.0/CookieClickerBackend.AssemblyInfo.cs
+++ CookieClickerBackend/obj/Debug/net8.0/CookieClickerBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CookieClickerBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a8ca04b2989458143a446f49ec48dea01e74710")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4d11a67216a434000557b8421606cff516eb443")]
 [assembly: System.Reflection.AssemblyProductAttribute("CookieClickerBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CookieClickerBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
